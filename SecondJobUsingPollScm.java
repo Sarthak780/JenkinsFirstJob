81b0dc8c16df4895c8029bf748052a1f94172051
sarthak <sarthakk179@gmail.com>
2024-02-25 13:25:21 +0530

    Commiting for testing pollScm in jenkins

---
+++ SecondJobUsingPollScm.java
@@ -0,0 +1,5 @@
+public class SecondJobUsingPollScm{
+	public static void main(String[] args){
+		System.out.print("My second job ran successfully after doing some changes and commiting it to github triggers the job after 1 minute as 1 minute poll time is set in jenkins");	
+	}
+}
